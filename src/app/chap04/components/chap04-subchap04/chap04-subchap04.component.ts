16555ea1eaa11493e3dc00969a472a58e8adeb69
Vladimir-Ioan <36692556+vladimirioan82@users.noreply.github.com>
2018-07-23 02:14:01 +0300

    don't know

--- src/app/chap04/components/chap04-subchap04/chap04-subchap04.component.ts
+++ src/app/chap04/components/chap04-subchap04/chap04-subchap04.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  ViewChildren, 
+  QueryList, 
+  AfterViewInit,
+  ContentChildren,
+  AfterContentInit
+} from '@angular/core';
 
 import { IPerson } from '../data.structures';
+import { Chap04NewPersonComponent } from '../chap04-new-person/chap04-new-person.component';
+import { Chap04ListOfPersonsComponent } from '../chap04-list-of-persons/chap04-list-of-persons.component';
+import { Chap04ListOnePersonComponent } from '../chap04-list-one-person/chap04-list-one-person.component';
 
 @Component({
   selector: 'fg-app-chap04-subchap04',
   templateUrl: './chap04-subchap04.component.html',
   styleUrls: ['./chap04-subchap04.component.scss']
 })
-export class Chap04Subchap04Component implements OnInit {
+export class Chap04Subchap04Component implements 
+  OnInit,
+  AfterViewInit,
+  AfterContentInit {
   public listOfPersons: Array<IPerson>;
 
+  @ViewChildren(Chap04NewPersonComponent)
+    public newPersonFromView: QueryList<Chap04NewPersonComponent>;
+
+  @ContentChildren(Chap04ListOnePersonComponent)
+    public listOfPersonsFromContent: QueryList<Chap04ListOnePersonComponent>;
+    
   constructor() { 
     this.listOfPersons = [
       {
@@ -34,6 +54,23 @@ export class Chap04Subchap04Component implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    console.log('In ngAfterViewChecked from Chap04Subchap04Component');
+    console.log('Variable persons has changed.');
+    console.log(this.newPersonFromView.toArray());
+    console.log('----------------------------------------');
+  }
+
+  ngAfterContentInit() {
+    console.log('In ngAfterContentInit from Chap04Subchap04Component');
+    console.log('Variable persons has changed.');
+    console.log(this.listOfPersonsFromContent.toArray());
+    this.listOfPersonsFromContent.toArray().forEach((element: any) => {
+      console.log(element);
+    })
+    console.log('----------------------------------------');
   }
 
   public addNewPerson(person: IPerson) {
