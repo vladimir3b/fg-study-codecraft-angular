12df490d61cfeb67a5cdff5b2e71e850e0ece458
Vladimir-Ioan <36692556+vladimirioan82@users.noreply.github.com>
2018-07-23 00:14:01 +0300

    components final

--- src/app/chap04/components/chap04-list-of-persons/chap04-list-of-persons.component.ts
+++ src/app/chap04/components/chap04-list-of-persons/chap04-list-of-persons.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  Output, 
+  EventEmitter, 
+  OnChanges, 
+  DoCheck,
+  SimpleChanges,
+  AfterViewChecked,
+  AfterContentInit,
+  AfterContentChecked
+} from '@angular/core';
+
 import { IPerson } from '../data.structures';
 
 @Component({
@@ -6,15 +19,84 @@ import { IPerson } from '../data.structures';
   templateUrl: './chap04-list-of-persons.component.html',
   styleUrls: ['./chap04-list-of-persons.component.scss']
 })
-export class Chap04ListOfPersonsComponent implements OnInit {
+export class Chap04ListOfPersonsComponent implements 
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterViewChecked,
+  AfterContentInit,
+  AfterContentChecked {
 
-  @Input() public persons: Array<IPerson>;
+  @Input() public persons: Array<IPerson> = [];
   @Output('onDeleteAllUsers') public usersDeletion: EventEmitter<number> =
     new EventEmitter();
 
-  constructor() { }
+  constructor() { 
+    console.log('In the constructor');
+    console.log('Angular created component Chap04ListOfPersonsComponent');
+    console.log(`At this step 'persons' has ${this.persons.length} elements.`);
+    console.log('----------------------------------------');
+  }
 
   ngOnInit() {
+    console.log('In ngOnInit');
+    console.log('Component Chap04ListOfPersonsComponent has been initializes.');
+    console.log(`Now 'persons' has ${this.persons.length} elements.`);
+    console.log('----------------------------------------');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    /** 
+     *  If the input variable is changed in other place than the parent component
+     *  this function won't trigger.
+    */
+    console.log('In ngOnChanges');
+    console.log('Variable persons has changed.');
+    console.log(`Now 'persons' has ${this.persons.length} elements.`);
+    console.log('Changes');
+    console.log('--------');
+    for(let key in changes) {
+      console.log(`${key} changed.
+      Current: ${changes[key].currentValue}.
+      Previous: ${changes[key].previousValue}`);
+    }
+    console.log('----------------------------------------');
+  }
+
+  ngDoCheck() {
+    /**
+     * This function will trigger at any change in the components.
+     */
+    console.log('In ngDoCheck');
+    console.log('Variable persons has changed.');
+    console.log(`Now 'persons' has ${this.persons.length} elements.`);
+    console.log('----------------------------------------');
+  }
+
+  ngAfterViewChecked() {
+    console.log('In ngAfterViewChecked');
+    console.log('Variable persons has changed.');
+    console.log(`Now 'persons' has ${this.persons.length} elements.`);
+    console.log('----------------------------------------');
+  }
+
+  ngAfterViewInit () {
+    console.log('In ngAfterViewInit');
+    console.log('Component Chap04ListOfPersonsComponent\'s view has been initializes.');
+    console.log('----------------------------------------');
+  }
+
+  ngAfterContentInit() {
+    console.log('In ngAfterContentInit');
+    console.log('Component Chap04ListOfPersonsComponent\'s content projectio\'s view has been initializes.');
+    console.log('----------------------------------------');
+  }
+
+  ngAfterContentChecked() {
+    console.log('In ngAfterContentChecked');
+    console.log('Variable persons has changed.');
+    console.log(`Now 'persons' has ${this.persons.length} elements.`);
+    console.log('----------------------------------------');
   }
 
   public deleteUsers(): void {
--- src/app/chap04/components/chap04-list-one-person/chap04-list-one-person.component.ts
+++ src/app/chap04/components/chap04-list-one-person/chap04-list-one-person.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { 
+  Component,    
+  Output, 
+  EventEmitter, 
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'fg-app-chap04-list-one-person',
   templateUrl: './chap04-list-one-person.component.html',
   styleUrls: ['./chap04-list-one-person.component.scss']
 })
-export class Chap04ListOnePersonComponent implements OnInit {
+export class Chap04ListOnePersonComponent implements OnDestroy {
 
   @Output('onDeleteOnePerson') public deletePerson: EventEmitter<boolean> = 
     new EventEmitter();
   constructor() { }
 
-  ngOnInit() {
+  ngOnDestroy() {
+    console.log('In the ngOnDestroy');
+    console.log('They destroyed a Chap04ListOnePersonComponent component');
+    console.log('----------------------------------------');
   }
 
   deleteThis() {
