bcfed2b42e4535a6f18b6d367c7a3b5459ac4e91
Vladimir-Ioan <36692556+vladimirioan82@users.noreply.github.com>
2018-07-23 01:18:24 +0300

    something strange

--- src/app/chap04/components/chap04-list-of-persons/chap04-list-of-persons.component.ts
+++ src/app/chap04/components/chap04-list-of-persons/chap04-list-of-persons.component.ts
@@ -9,10 +9,17 @@ import {
   SimpleChanges,
   AfterViewChecked,
   AfterContentInit,
-  AfterContentChecked
+  AfterContentChecked,
+  ViewChild,
+  ViewChildren,
+  QueryList,
+  ContentChild,
+  AfterViewInit
 } from '@angular/core';
 
 import { IPerson } from '../data.structures';
+import { Chap04ListOnePersonComponent } from '../chap04-list-one-person/chap04-list-one-person.component';
+import { Chap04Subchap04Component } from '../chap04-subchap04/chap04-subchap04.component';
 
 @Component({
   selector: 'fg-app-chap04-list-of-persons',
@@ -24,12 +31,19 @@ export class Chap04ListOfPersonsComponent implements
   OnChanges,
   DoCheck,
   AfterViewChecked,
+  AfterViewInit,
   AfterContentInit,
   AfterContentChecked {
 
   @Input() public persons: Array<IPerson> = [];
   @Output('onDeleteAllUsers') public usersDeletion: EventEmitter<number> =
     new EventEmitter();
+  @ViewChild(Chap04ListOnePersonComponent) listOnePersonViewChild: Chap04ListOnePersonComponent;
+  @ViewChildren(Chap04ListOnePersonComponent) listOnePersonViewChildren: 
+    QueryList<Chap04ListOnePersonComponent>;  
+
+  @ContentChild(Chap04ListOfPersonsComponent) listOnePersonContentChild: Chap04ListOnePersonComponent;
+
 
   constructor() { 
     console.log('In the constructor');
@@ -80,24 +94,30 @@ export class Chap04ListOfPersonsComponent implements
     console.log('----------------------------------------');
   }
 
-  ngAfterViewInit () {
-    console.log('In ngAfterViewInit');
-    console.log('Component Chap04ListOfPersonsComponent\'s view has been initializes.');
-    console.log('----------------------------------------');
-  }
-
-  ngAfterContentInit() {
-    console.log('In ngAfterContentInit');
-    console.log('Component Chap04ListOfPersonsComponent\'s content projectio\'s view has been initializes.');
-    console.log('----------------------------------------');
-  }
-
   ngAfterContentChecked() {
     console.log('In ngAfterContentChecked');
     console.log('Variable persons has changed.');
     console.log(`Now 'persons' has ${this.persons.length} elements.`);
     console.log('----------------------------------------');
   }
+
+  ngAfterViewInit () {
+    console.log('In ngAfterViewInit');
+    console.log('Component Chap04ListOfPersonsComponent\'s view has been initialized.');
+    console.log(this.listOnePersonViewChild);
+    console.log(this.listOnePersonViewChildren.toArray());
+    console.log('----------------------------------------');
+  }
+
+  ngAfterContentInit() {
+    console.log('In ngAfterContentInit');
+    console.log('Component Chap04ListOfPersonsComponent content project\'s view has been initialized.');
+    console.log(this.listOnePersonContentChild);
+    // console.log(this.listOnePersonViewChildren.toArray());
+    console.log('----------------------------------------');
+  }
+
+
 
   public deleteUsers(): void {
     this.usersDeletion.emit(0);
