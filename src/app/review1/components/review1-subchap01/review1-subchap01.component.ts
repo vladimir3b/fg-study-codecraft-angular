77675042ee8d8b5f4eaef94704a7d6016b3b35ed
Vladimir-Ioan <36692556+vladimirioan82@users.noreply.github.com>
2018-08-01 00:27:13 +0300

    decorators again

--- src/app/review1/components/review1-subchap01/review1-subchap01.component.ts
+++ src/app/review1/components/review1-subchap01/review1-subchap01.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Review1Subchap01Component implements OnInit {
 
+  public message: string = '';
+
   constructor() { }
 
   ngOnInit() {
+    this.reviewDecorators();
   }
 
+  public reviewDecorators():void {
+    
+    function logger(target) {
+      target.prototype.log = (message: string) => {
+        console.log(message);
+      }
+    }
+
+    @logger
+    class Test01 {      
+      private _prop1: string = 'I try to learn decorators.';
+      public prop1(): string {
+        return this._prop1;
+      }
+    }
+
+    let test01: Test01 = new Test01();
+
+    (<any>test01).log(test01.prop1());
+
+    // console.log(test01);
+    
+    
+  }
 }
