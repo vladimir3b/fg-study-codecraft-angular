c7ab0ec6d8662ba094105721a3f41b57010427b3
Vladimir-Ioan <36692556+vladimirioan82@users.noreply.github.com>
2018-08-06 10:53:20 +0300

    pipes

--- src/app/chap08/components/chap08-subchap01/chap08-subchap01.component.ts
+++ src/app/chap08/components/chap08-subchap01/chap08-subchap01.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'fg-app-chap08-subchap01',
   templateUrl: './chap08-subchap01.component.html',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Chap08Subchap01Component implements OnInit {
 
-  constructor() { }
+  public currentDate: Date = new Date();
+  public person = {
+    firstName: 'John',
+    lastName: 'Smith',
+    age: 45,
+    sex: 'male',
+    retired: false
+  }
+  public vector: Array<string>; 
+  public min: number;
+  public max: number;
+
+  constructor() { 
+    this.vector = [];
+  }
 
   ngOnInit() {
+    for (let i: number = 0; i <= 100; i++){
+      this.vector.push(`person ${i + 1}`);
+    }
+    this.min = 1;
+    this.max = this.vector.length;
   }
 
 }
--- src/app/chap08/components/chap08-subchap02/chap08-subchap02.component.ts
+++ src/app/chap08/components/chap08-subchap02/chap08-subchap02.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+import { myMath } from '../../../others/libraries/typescript/math';
+
 @Component({
   selector: 'fg-app-chap08-subchap02',
   templateUrl: './chap08-subchap02.component.html',
   styleUrls: ['./chap08-subchap02.component.scss']
 })
 export class Chap08Subchap02Component implements OnInit {
-
-  constructor() { }
+  public message1: string = 'Wait a litte...';
+  public message2: string = 'Wait a litte...';
+  public promise: Promise<number>;
+  
+  constructor() {
+    this.promise = this.createPromise();
+  }
 
   ngOnInit() {
+    this.createPromise()
+      .then((age: number) => this.message1 = `John is ${age} years old now.`)
+      .catch((error: Error) => console.error(error.message));      
+  }
+
+  public createPromise(): Promise<number> {
+    return new Promise<number>((resolve, reject) => {
+      let hasAnError: boolean = myMath.randomInteger(5, 750) % myMath.randomInteger(2, 7) === 0;     
+      setTimeout(() => {
+        try {
+          if (hasAnError) {
+            throw new Error('There was an error!');
+          }
+        } 
+        catch(error) {
+          reject(error);
+        }
+        resolve(myMath.randomInteger(34, 75));
+      }, myMath.randomInteger(1500, 2500)
+     )
+    });
   }
 
 }
