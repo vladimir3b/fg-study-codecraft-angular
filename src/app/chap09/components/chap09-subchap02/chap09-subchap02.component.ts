37fa16dc74ce6df7478e13c593959a8b07bc5977
VladimirIoan <vladimir_3b@hotmail.com>
2018-08-13 16:08:54 +0300

    valdiate forms...

--- src/app/chap09/components/chap09-subchap02/chap09-subchap02.component.ts
+++ src/app/chap09/components/chap09-subchap02/chap09-subchap02.component.ts
@@ -1,6 +1,16 @@
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
+interface IValidator {
+  isValid: boolean,
+  message: string
+}
+
+interface IInvalidator {
+  isInvalid: boolean,
+  message: string
+}
+
 @Component({
   selector: 'fg-app-chap09-subchap02',
   templateUrl: './chap09-subchap02.component.html',
@@ -8,7 +18,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Chap09Subchap02Component implements OnInit {
 
-  public showDebug = true;
+  public showDebug = false;
   public languages: Array<string> = [];
   public myForm: FormGroup;
 
@@ -44,5 +54,51 @@ export class Chap09Subchap02Component implements OnInit {
     });
   }
 
+  public validate(key: string): IValidator {
+
+    let validator: IValidator = {
+      isValid: false,
+      message: ''
+    };
+
+    switch (key) {
+      case 'email': 
+        validator.isValid = 
+          (<any>this.myForm.controls.account).controls.email.valid  && 
+          ((<any>this.myForm.controls.account).controls.email.dirty ||
+          (<any>this.myForm.controls.account).controls.email.touched);   
+        validator.message = 'The email seems to be correct.';
+        break;     
+
+    }
+
+    return validator;
+  }
+
+  public invalidate(key: string): IInvalidator {
+
+    let invalidator: IInvalidator = {
+      isInvalid: true,
+      message: ''
+    };
+
+    switch (key) {
+      case 'email': 
+        invalidator.isInvalid =
+          (<any>this.myForm.controls.account).controls.email.invalid  && 
+          ((<any>this.myForm.controls.account).controls.email.dirty ||
+          (<any>this.myForm.controls.account).controls.email.touched);      
+          if ((<any>this.myForm.controls.account).controls.email.errors) {
+            invalidator.message = 
+              (<any>this.myForm.controls.account).controls.email.errors.required ? 
+              'The email is required' : 'The email must contain at least a @ character.';
+          } else {
+            invalidator.message ='';
+          }
+          
+        break;
+    } 
+    return invalidator
+  }
 
 }
