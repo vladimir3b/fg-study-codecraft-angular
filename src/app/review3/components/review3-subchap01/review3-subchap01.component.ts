0f765c140861c646659fba052a760162640dadf6
Vladimir-Ioan <36692556+vladimirioan82@users.noreply.github.com>
2018-08-16 08:13:42 +0300

    forms review1.1

--- src/app/review3/components/review3-subchap01/review3-subchap01.component.ts
+++ src/app/review3/components/review3-subchap01/review3-subchap01.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'fg-app-review3-subchap01',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Review3Subchap01Component implements OnInit {
 
+  public messageForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.messageForm = new FormGroup({});
+  }
+
+  public onSubmit(): void {
+    console.log('Something submitted...');
+    if (this.messageForm.valid) {
+      console.log(this.messageForm.value);
+    }    
   }
 
 }
