292414af45e3b27cf071f4055afd5222c54c1a0e
Vladimir-Ioan <36692556+vladimirioan82@users.noreply.github.com>
2018-08-20 00:34:06 +0300

    review decorators

--- src/app/review1/components/review1-subchap01/review1-subchap01.component.ts
+++ src/app/review1/components/review1-subchap01/review1-subchap01.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { timeInterval } from '../../../../../node_modules/rxjs/operators';
+
+type Constructor = { new (...args: Array<any>): {} };
+interface IDataTime  {
+  auto: boolean;
+  time?: string;
+  date?: string;
+}
 
 @Component({
   selector: 'fg-app-review1-subchap01',
@@ -17,23 +25,85 @@ export class Review1Subchap01Component implements OnInit {
 
   public reviewDecorators():void {
     
-    function logger(target) {
-      target.prototype.log = (message: string) => {
-        console.log(message);
-      }
+    let message: string = '';
+
+
+
+    // these functions are decorators:
+
+    function simpleDecorator(target: Function) {
+      message += 'This is the easiest decorator possible...';
     }
 
-    @logger
-    class Test01 {      
-      private _prop1: string = 'I try to learn decorators.';
-      public prop1(): string {
-        return this._prop1;
+    function gun<T extends Constructor>(target: T): T {//create decorator gun
+      return class extends target {
+        type: string;
+        color: string;
+        caliber: string = '9mm';
+        log (message: string): void {
+          console.log(`A new ${this.type} with color ${this.color} and caliber ${this.caliber} has been added. ${message}`)
+        }
       }
+    }// end of decorator @gun
+
+    function color<T extends Constructor>(target: T): void {
+      target.prototype.otherColor = (): string => 'The sun has color yellow :D...'; 
     }
 
-    let test01: Test01 = new Test01();
+    function timeOfInstantiation(dataTime: IDataTime): Function {
+      
+      let now: Date = new Date();
+      let time: string = (dataTime.auto || !dataTime.time) ? 
+        `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}` : dataTime.time;
+      let date: string = (dataTime.auto || !dataTime.date) ? 
+        `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}` : dataTime.date;    
 
-    (<any>test01).log(test01.prop1());
-       
+      return function <T extends Constructor>(target: T): T {
+        return class extends target {
+          public time: string = time;
+          public date: string = date;
+          public logDate = () => console.log(`This was instantiated at ${this.time} on ${this.date}.`)
+        }
+      }
+
+    }
+
+
+    // here we use those decorators:
+
+    @simpleDecorator
+    @gun
+    @color
+    @timeOfInstantiation({
+      auto: false,
+      time: '12:25:37'
+    })
+    class MyClass {
+      constructor (public type: string, public color: string){}
+    }
+
+    @timeOfInstantiation({
+      auto: true
+    })
+    class Person {
+      constructor(
+        public firstName: string,
+        public lastName: string,
+        public sex: boolean,
+        public age: number
+      ) {}
+    }
+
+    let myObject: MyClass = new MyClass('gun', 'pink');
+
+    message += ` I have a ${myObject.type} with color ${myObject.color} and  caliber ${(<any>myObject).caliber}.`;
+    (<any>myObject).log('We don\'t need no education, we have guns... a lot of guns...');
+    message += ` ${(<any>myObject).otherColor()} `;
+    (<any>myObject).logDate();
+
+    let myMan: Person = new Person('John', 'Smith', false, 45);
+    (<any>myMan).logDate();
+
+    this.message = message;
   }
 }
