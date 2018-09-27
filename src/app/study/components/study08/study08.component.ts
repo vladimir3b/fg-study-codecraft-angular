b325f261e9cd46eef93c88224504164aa5d23c88
VladimirIoan <vladimir_3b@hotmail.com>
2018-09-27 19:08:34 +0300

    data binding

--- src/app/study/components/study08/study08.component.ts
+++ src/app/study/components/study08/study08.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IPlace {
+  city: string;
+  state: string;
+  country: string;
+  zipCode: string;
+  visited: boolean;
+  toString?: () => string;
+}
+
 @Component({
   selector: 'fg-app-study08',
   templateUrl: './study08.component.html',
@@ -7,9 +16,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Study08Component implements OnInit {
 
-  constructor() { }
+  public myCity: string;
+  public myLocation: IPlace;
+  public yourLocation: IPlace;
+
+  public successButton = {
+    text: 'This is a success button',
+    style: 'btn btn-success'
+  }
+  public dangerButton = {
+    text: 'This is a danger button',
+    style: 'btn btn-danger'
+  }
+  public changingButton: any = {};
+  public isDisabled: boolean;
+  public someHTMLCode: string;
+  public color: string = 'red';
+  public size: string = '20px';
+  public fontStyle: string = 'italic';
+
+  constructor() { 
+    this.isDisabled = true;
+    this.changingButton = this.successButton;
+    this.myCity = 'Lisbon';
+    this.myLocation = {
+      city: 'Bucharest',
+      state: 'Muntenia',
+      country: 'Romania',
+      zipCode: 'RO456789',
+      visited: true
+    }
+    setTimeout(() => {
+      this.isDisabled = false;
+      this.changingButton = this.dangerButton;
+      this.yourLocation = {
+        city: 'Sibiu',
+        state: 'Transilvania',
+        country: 'Romania',
+        zipCode: 'RO55879',
+        visited: true,
+        toString: function() {
+          // arrow function doesn't work here 
+          return `city: ${this.city}, state: ${this.state}, country: ${this.country}`
+        }
+      }
+    }, 3500);
+  }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.myCity = 'Valencia';
+    }, 2000);
+    setTimeout(() => {
+      this.someHTMLCode = `John Smith is a <strong>big</strong> guy.`;
+    }, 4000);
+  }
+
+  public showANewMessage(): string {
+    //this.myLocation.city = 'Pitesti'; - you cannot change a value while interpolating
+    return `I don't like my city, ${this.myLocation.city} and I want to live in ${this.myCity}.`;
+  }
+
+  get showANewMessageWithAGetter(): string {
+    return `You don't like your city, ${this.yourLocation && this.yourLocation.city} and you want to live in ${this.myCity}.`;
   }
 
 }
